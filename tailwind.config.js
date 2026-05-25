e34fc2871ef316960ab163c5f88826ad111833c0
casave5 <carlosmsv@gmail.com>
2026-05-25 01:07:49 -0400

    Primer Commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./App.jsx",
+    "./axie_card_derby.tsx",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
